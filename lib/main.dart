6277f38b38d9a66b7cbeab64153f2d679183aca4
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-09-05 08:36:08 +0530

    77.styling a container

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,12 @@ class MyHomePage extends StatelessWidget {
                     child: Row(
                       children: [
                         Container(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 15),
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2),
+                          ),
                           child: Text(
                             tx.amount.toString(),
                           ),
