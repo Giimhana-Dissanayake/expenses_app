cd72c4b1f27b0990a412d949832669b2ad273cb9
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-09-05 12:19:54 +0530

    89.making the list scrollable - using SingleChildScrollView inside a container with fixed height

--- lib/main.dart
+++ lib/main.dart
@@ -21,21 +21,19 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Flutter App'),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                child: Card(
-                  elevation: 5,
-                  color: Colors.indigoAccent,
-                  child: Text("CHART!"),
-                ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                elevation: 5,
+                color: Colors.indigoAccent,
+                child: Text("CHART!"),
               ),
-              UserTransactions(),
-            ],
-          ),
+            ),
+            UserTransactions(),
+          ],
         ));
   }
 }
