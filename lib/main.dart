20707e9d07f748399edd73f43f1525e486042205
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-09-05 08:13:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import './transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: "New shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Weekly Groceries",
+      amount: 16.53,
+      date: DateTime.now(),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                elevation: 5,
+                color: Colors.indigoAccent,
+                child: Text("CHART!"),
+              ),
+            ),
+            Column(
+              children: transactions.map(
+                (tx) {
+                  return Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Text(
+                            tx.amount.toString(),
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Text(tx.title),
+                            Text(tx.amount.toString())
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          ],
+        ));
+  }
+}
